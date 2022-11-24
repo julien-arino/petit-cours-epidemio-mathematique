4d9b5c19f6c704d8115b141fb178b1a40b960de6
Julien Arino <51942189+julien-arino@users.noreply.github.com>
2022-11-23 22:17:22 -0600

    Update

--- CODE/SIR_KMK_final_size.R
+++ CODE/SIR_KMK_final_size.R
@@ -10,7 +10,7 @@ uniroot(f = final_size, interval = c(0.05, 999))
 N0 = 1000
 I0 = 1
 S0 = N0-I0
-R_0 = 2.4
+R_0 = 0.8
 
 S = seq(0.1, S0, by = 0.1)
 fs = final_size(S, S0 = S0, I0 = I0, R_0 = R_0)
@@ -19,11 +19,11 @@ S_inf = uniroot(f = function(x) final_size(S_inf = x,
                                            R_0 = R_0),
                 interval = c(0.05, S0))
 
-png("../FIGS/KMK_final_size_R0_2p4.png", width = 1200, height = 800, res = 200)
+png("../FIGS/KMK_final_size_R0_0p8.png", width = 1200, height = 800, res = 200)
 plot(S, fs, type = "l", ylab = "Valeur de l'équation (8)")
 abline(h = 0)
 points(x = S_inf$root, y = 0, pch = 19)
-text(x = S_inf$root, y = 0, labels = "S_inf", adj = c(-0.25,-1))
+text(x = S_inf$root, y = 0, labels = "S_inf", adj = c(0.75,-1))
 dev.off()
-crop_figure("../FIGS/KMK_final_size_2p4.png")
+crop_figure("../FIGS/KMK_final_size_R0_0p8.png")
 
